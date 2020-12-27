15d9e4b97ba549d202c89511e3d9175f37355357
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2020-12-27 08:56:14 +0700

    day12: refactor code

--- mysolution/day12_nbody_problem/solve.py
+++ mysolution/day12_nbody_problem/solve.py
@@ -6,44 +6,45 @@ import math
 import os
 import re
 from collections.abc import Iterable, Iterator, Sequence
-from typing import Annotated, NamedTuple, TypeVar
+from typing import NamedTuple, TypeVar
 
 T = TypeVar('T')
+StateVector = Sequence['StateScalar']
+Period = int
+Offset = int
 
-Position = list[int]
-VectoredState = Sequence['ScalarState']
 COORDS_RE = re.compile(r'<x=(-?\d+), y=(-?\d+), z=(-?\d+)>')
 
 
 def main():
     this_dir = os.path.dirname(os.path.abspath(__file__))
     input_file = os.path.join(this_dir, 'input.txt')
-    positions = read_input_file(input_file)
+    initial_vectors = read_input_file(input_file)
 
     # Part 1
-    vectors = simulate(positions, repeat=1000)
-    p1_answer = sum(energy(v) for v in vectors)
+    updated_vectors = simulate(initial_vectors, repeat=1000)
+    p1_answer = sum(energy(nd_state) for nd_state in updated_vectors)
     print(p1_answer)
 
     # Part 2
-    p2_answer = period_simulation_repeated(positions)
+    p2_answer = find_simulation_period(initial_vectors)
     print(p2_answer)
 
 
-class ScalarState(NamedTuple):
+class StateScalar(NamedTuple):
     """
-    One-dimensional planet state (position and velocity on a single axis).
+    Represents a planet physical state (position and velocity) in one dimension.
     """
     pos: int
     vel: int = 0
 
-    def next_state(self, others: Iterable[ScalarState]) -> ScalarState:
+    def next_state(self, others: Iterable[StateScalar]) -> StateScalar:
         acl = sum(self.acl_pulled_by(p) for p in others)
         next_vel = self.vel + acl
         next_pos = self.pos + next_vel
-        return ScalarState(next_pos, next_vel)
+        return StateScalar(next_pos, next_vel)
 
-    def acl_pulled_by(self, other: ScalarState) -> int:
+    def acl_pulled_by(self, other: StateScalar) -> int:
         if self.pos < other.pos:
             return 1
         elif self.pos > other.pos:
@@ -52,51 +53,50 @@ class ScalarState(NamedTuple):
             return 0
 
 
-def simulate(positions: Sequence[Position], repeat: int) -> list[VectoredState]:
-    multidimensional_scalars = []
-    for scalars in zip(*positions):
-        scalars = [ScalarState(pos=s) for s in scalars]
-        scalars = functools.reduce(lambda s, _: simulate_next_linear(s), range(repeat), scalars)
-        multidimensional_scalars.append(scalars)
-    vectors = list(zip(*multidimensional_scalars))
-    return vectors
+def simulate(initial_states: Sequence[StateVector], repeat: int) -> list[StateVector]:
+    transposed_samples = []
+    for sampled_scalars in zip(*initial_states):
+        updated_sampled_scalars = functools.reduce(
+            lambda ss, _: simulate_next_linear(ss),
+            range(repeat), sampled_scalars,
+        )
+        transposed_samples.append(updated_sampled_scalars)
+    updated_states = list(zip(*transposed_samples))
+    return updated_states
 
 
-def period_simulation_repeated(positions: Sequence[Position]) -> int:
+def find_simulation_period(initial_states: Sequence[StateVector]) -> int:
     multidimensional_periods = []
-    for scalars in zip(*positions):
-        scalars = [ScalarState(pos=s) for s in scalars]
-        period, offset = simulate_linear_until_repeat(scalars)
+    for sampled_scalars in zip(*initial_states):
+        period, offset = simulate_linear_until_repeat(sampled_scalars)
         assert offset == 0
         multidimensional_periods.append(period)
     return math.lcm(*multidimensional_periods)
 
 
-def simulate_linear_until_repeat(
-        scalars: Sequence[ScalarState],
-) -> tuple[Annotated[int, 'period'], Annotated[int, 'offset']]:  # noqa: F821
-    recorded_offsets = {tuple(scalars): 0}
+def simulate_linear_until_repeat(sampled_scalars: Sequence[StateScalar]) -> tuple[Period, Offset]:
+    sampled_scalars = tuple(sampled_scalars)
+    recorded_offsets = {sampled_scalars: 0}
     for time in itertools.count(start=1):
-        scalars = simulate_next_linear(scalars)
-        tuple_scalars = tuple(scalars)
-        if tuple_scalars in recorded_offsets:
-            offset = recorded_offsets[tuple_scalars]
+        sampled_scalars = tuple(simulate_next_linear(sampled_scalars))
+        if sampled_scalars in recorded_offsets:
+            offset = recorded_offsets[sampled_scalars]
             period = time - offset
             return period, offset
-        recorded_offsets[tuple_scalars] = time
+        recorded_offsets[sampled_scalars] = time
 
 
-def simulate_next_linear(scalars: Sequence[ScalarState]) -> list[ScalarState]:
-    next_states = [
+def simulate_next_linear(sampled_scalars: Sequence[StateScalar]) -> Sequence[StateScalar]:
+    updated_sampled_scalars = [
         primary.next_state(others)
-        for primary, others in one_and_rest(scalars)
+        for primary, others in one_and_rest(sampled_scalars)
     ]
-    return next_states
+    return updated_sampled_scalars
 
 
-def energy(vectored_state: VectoredState) -> int:
-    pot_energy = sum(abs(singular.pos) for singular in vectored_state)
-    kin_energy = sum(abs(singular.vel) for singular in vectored_state)
+def energy(vector: StateVector) -> int:
+    pot_energy = sum(abs(scalar.pos) for scalar in vector)
+    kin_energy = sum(abs(scalar.vel) for scalar in vector)
     return pot_energy * kin_energy
 
 
@@ -110,17 +110,17 @@ def one_and_rest(values: Sequence[T]) -> Iterator[tuple[T, Iterator[T]]]:
         yield primary, (other for _, other in with_others)
 
 
-def read_input_file(filename: str) -> list[Position]:
+def read_input_file(filename: str) -> list[StateVector]:
     """
-    Extracts a list of initial Jupiter's moon positions.
+    Extracts a list of initial states of Jupiter's moons.
     """
     with open(filename) as fobj:
-        positions = [parse_initial_state(line.strip()) for line in fobj]
-    return positions
+        initial_states = [parse_initial_state(line.strip()) for line in fobj]
+    return initial_states
 
 
-def parse_initial_state(raw: str) -> Position:
-    return [int(token) for token in COORDS_RE.fullmatch(raw).groups()]
+def parse_initial_state(raw: str) -> StateVector:
+    return [StateScalar(pos=int(token)) for token in COORDS_RE.fullmatch(raw).groups()]
 
 
 if __name__ == '__main__':
